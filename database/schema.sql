b28de4b1495e8e80f61e939cc2aa4926eceea17a
Alihan Esen <esenal22@itu.edu.tr>
2024-11-09 23:07:11 +0300

    Added tables and ERD
    Added more tables with references to the database and added the current Entity Relationship Diagram as an image

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `affinities` (
 	`id` int AUTO_INCREMENT NOT NULL,
 	`name` varchar(255) NOT NULL,
-	`affinity_passive_id` int NOT NULL,
+	`affinity_passive_id` int,
 	PRIMARY KEY (`id`)
 );
 
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `weapons_w_affinities` (
 	`main_weapon_id` int NOT NULL,
 	`affinity_id` int NOT NULL,
 	`name` varchar(255) NOT NULL,
-	`scaling` bigint NOT NULL,
+	`scaling` varchar(255),
 	PRIMARY KEY (`id`)
 );
 
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `effects` (
 CREATE TABLE IF NOT EXISTS `weapon_skills` (
 	`id` int AUTO_INCREMENT NOT NULL,
 	`name` varchar(255) NOT NULL,
-	`aow_id` int NOT NULL,
+	`aow_id` int,
 	PRIMARY KEY (`id`)
 );
 
@@ -62,6 +62,68 @@ CREATE TABLE IF NOT EXISTS `weapon_groups` (
 	PRIMARY KEY (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `regions` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `locations` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`name` varchar(255) NOT NULL,
+	`region` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `npcs` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`encounter_id` int NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`hp` int NOT NULL,
+	`human` bool NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `armors` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`set_id` int NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`equip_slot` varchar(255) NOT NULL,
+	`desc` text NOT NULL,
+	`weight` float NOT NULL,
+	`can_alter` bool NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `npc_encounters` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`name` varchar(255) NOT NULL,
+	`hp` int NOT NULL,
+	`runes` int NOT NULL,
+	`location_id` int NOT NULL,
+	`only_night` bool NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `npc_gear` (
+	`id` int NOT NULL UNIQUE,
+	`right_weapon_id` int,
+	`right_weapon_skill_id` int,
+	`left_weapon_id` int,
+	`left_weapon_skill_id` int,
+	`armor_head_id` int,
+	`armor_body_id` int,
+	`armor_arms_id` int,
+	`armor_legs_id` int,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `armor_sets` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
 ALTER TABLE `affinities` ADD CONSTRAINT `affinities_fk2` FOREIGN KEY (`affinity_passive_id`) REFERENCES `effects`(`id`);
 ALTER TABLE `weapons_w_affinities` ADD CONSTRAINT `weapons_w_affinities_fk1` FOREIGN KEY (`main_weapon_id`) REFERENCES `weapons`(`id`);
 
@@ -76,3 +138,26 @@ ALTER TABLE `weapons` ADD CONSTRAINT `weapons_fk6` FOREIGN KEY (`hidden_effect_i
 
 ALTER TABLE `weapons` ADD CONSTRAINT `weapons_fk7` FOREIGN KEY (`default_skill_id`) REFERENCES `weapon_skills`(`id`);
 ALTER TABLE `ashes_of_war` ADD CONSTRAINT `ashes_of_war_fk2` FOREIGN KEY (`affinity_id`) REFERENCES `affinities`(`id`);
+
+
+ALTER TABLE `locations` ADD CONSTRAINT `locations_fk2` FOREIGN KEY (`region`) REFERENCES `regions`(`id`);
+ALTER TABLE `npcs` ADD CONSTRAINT `npcs_fk1` FOREIGN KEY (`encounter_id`) REFERENCES `npc_encounters`(`id`);
+ALTER TABLE `armors` ADD CONSTRAINT `armors_fk1` FOREIGN KEY (`set_id`) REFERENCES `armor_sets`(`id`);
+ALTER TABLE `npc_encounters` ADD CONSTRAINT `npc_encounters_fk4` FOREIGN KEY (`location_id`) REFERENCES `locations`(`id`);
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk0` FOREIGN KEY (`id`) REFERENCES `npcs`(`id`);
+
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk1` FOREIGN KEY (`right_weapon_id`) REFERENCES `weapons_w_affinities`(`id`);
+
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk2` FOREIGN KEY (`right_weapon_skill_id`) REFERENCES `weapon_skills`(`id`);
+
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk3` FOREIGN KEY (`left_weapon_id`) REFERENCES `weapons_w_affinities`(`id`);
+
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk4` FOREIGN KEY (`left_weapon_skill_id`) REFERENCES `weapon_skills`(`id`);
+
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk5` FOREIGN KEY (`armor_head_id`) REFERENCES `armors`(`id`);
+
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk6` FOREIGN KEY (`armor_body_id`) REFERENCES `armors`(`id`);
+
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk7` FOREIGN KEY (`armor_arms_id`) REFERENCES `armors`(`id`);
+
+ALTER TABLE `npc_gear` ADD CONSTRAINT `npc_gear_fk8` FOREIGN KEY (`armor_legs_id`) REFERENCES `armors`(`id`);
