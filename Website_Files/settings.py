caa42e63b28e8b76d32818e55344e2b41f773bef
Alihan Esen <esenal22@itu.edu.tr>
2024-12-16 00:30:26 +0300

    added configuration option for adding the database to the project
    sorry for the late commit

--- Website_Files/settings.py
+++ Website_Files/settings.py
@@ -1,2 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
+
 DEBUG = True
 PORT = 8080
+
+# Read environment variables
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+MYSQL_DB = os.getenv("MYSQL_DB", "game_database")
