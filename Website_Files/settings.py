0d444aba98358a0c79de5f07a4e72357aa71983a
Arif Eren Yoldaş <yoldas21@itu.edu.tr>
2025-02-03 20:14:50 +0300

    Fixes

--- Website_Files/settings.py
+++ Website_Files/settings.py
@@ -11,7 +11,7 @@ PORT = os.getenv("PORT", 8080)
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
-MYSQL_DB = os.getenv("MYSQL_DB", "blg317e_eldenring")
+MYSQL_DB = os.getenv("MYSQL_DB", "new_schema")
 
 # Secret key is loaded from .env file
 SECRET_KEY = os.getenv('SECRET_KEY', 'c9e225d2a6d53c85f83f595ce2a4f75b6c48e7f40c3b8452')
